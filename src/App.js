dfc74279c1f8a72b5f2d43c19bad3c16ed8b1a4d
zhenyafootball <fiatscudo2@gmail.com>
2022-01-12 11:42:53 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [users,setUsers] = useState([]);
+    let [posts,setPosts] = useState([]);
+    let [comments,setComments] = useState([]);
+
+   useEffect(()=>{
+      fetch(`https://jsonplaceholder.typicode.com/users`)
+          .then(value => value.json())
+          .then(value => {
+                 setUsers(value);
+          })
+
+       fetch(`https://jsonplaceholder.typicode.com/posts`)
+           .then(value => value.json())
+           .then(value => {
+               setPosts(value);
+
+           })
+
+       fetch(`https://jsonplaceholder.typicode.com/comments`)
+           .then(value => value.json())
+           .then(value => {
+               setComments(value)
+
+           })
+   }, []);
+    return (
+        <div>
+            <div className="user">
+         <ul>
+             {
+             users.map(value => <li key={value.id}> {value.name}</li>)
+             }
+         </ul>
+
+            <ul>
+                {
+                posts.map(value => <li key={value.id}> {value.title}</li>)
+                }
+            </ul>
+            </div>
+            <ul>
+                {
+                comments.map(value => <li key={value.id}> {value.body}</li>)
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default App;
